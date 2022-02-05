05b42158a9eae5f2190d11a05cb4c5a1b193c8f2
John Fernandez <jafernandez.tampabay@gmail.com>
2022-02-05 12:03:54 -0500

    various fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(function() {
+     // com
      $(".carousel").carousel( { interval: 2000 } );
      $("#carouselButton").click(function(){
        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
@@ -6,15 +7,13 @@ $(function() {
         $("#carouselButton").children("i").removeClass("fa-pause");
         $("#carouselButton").children("i").addClass("fa-play");
    
-       } else if ($("#carouselButton").children("i").hasClass("fa-play")) {
-         $(".carousel").carousel("cycle");
-         $("#carouselButton").children("i").removeClass("fa-play");
-         $("#carouselButton").children("i").addClass("fa-pause");               
        } else {
-         // This will keep it from breaking in case someone makes a bad script
-         // Wish I had some error reporting though.
          $(".carousel").carousel("cylce");
+         $("#carouselButton").children("i").removeClass("fa-play");
          $("#carouselButton").children("i").addClass("fa-pause");              
        }
+     
+     $
+       
    });
  });
